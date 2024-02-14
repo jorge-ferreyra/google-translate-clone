e9f80808254eb31a0bc0173c009f3afd53b19edf
Jorge Ferreyra <jorxfer@gmail.com>
2024-02-14 10:18:03 -0300

    Creating Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,79 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { type State, type Action } from './types.d'
+import { useEffect, useReducer } from 'react'
+
+const initialState: State = {
+  fromLanguage: 'auto',
+  toLanguage: 'en',
+  fromText: '',
+  result: '',
+  loading: false
+}
+
+function reducer (state: State, action: Action) {
+  const { type } = action
+
+  if (type === 'INTERCHANGE_LANGUAGES') {
+    return {
+      ...state,
+      fromLanguage: state.toLanguage,
+      toLanguage: state.fromLanguage
+    }
+  }
+
+  if (type === 'SET_FROM_LANGUAGE') {
+    return {
+      ...state,
+      fromLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_TO_LANGUAGE') {
+    return {
+      ...state,
+      toLanguage: action.payload
+    }
+  }
+
+  if (type === 'SET_FROM_TEXT') {
+    return {
+      ...state,
+      loading: true,
+      fromText: action.payload,
+      result: ''
+    }
+  }
+
+  if (type === 'SET_RESULT') {
+    return {
+      ...state,
+      loading: false,
+      result: action.payload
+    }
+  }
+
+  return state
+}
 
 function App() {
+  const [{
+    fromLanguage,
+    toLanguage,
+    fromText,
+    result,
+    loading
+  }, dispatch] = useReducer(reducer, initialState)
+
+  console.log(fromLanguage)
 
   return (
     <>
       <div className='app'>
         <h1>Google Translate</h1>
+        <button onClick={() => {
+          dispatch({ type: 'SET_FROM_LANGUAGE', payload: 'es' })
+        }}>Cambiar a español</button>
       </div>
     </>
   )
